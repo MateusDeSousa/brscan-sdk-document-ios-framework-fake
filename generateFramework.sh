cd4ec958b02a1818352660ada76ffd42c6190f0e
Mateus Sousa <mateusdevsousa@gmail.com>
2022-08-29 09:05:33 -0300

    Adiciona script para facilitar geração de framework

---
+++ generateFramework.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xcodegen
+pod install
+xcodebuild archive -workspace brscan_sdk_documento_ios_fake.xcworkspace -scheme brscan_sdk_documento_ios_fake -destination="iOS Simulator" -archivePath tmp/iossimulator.xcarchive -derivedDataPath tmp/iphoneos -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ENABLE_BITCODE=YES
+rm -rf brscan_sdk_documento_ios_fake.framework
+mv tmp/iossimulator.xcarchive/Products/Library/Frameworks/brscan_sdk_documento_ios_fake.framework .
